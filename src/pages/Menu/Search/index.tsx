3a04c2d014014633e2cfd4a062422d9024a99646
Vinicius Santos <vinicius.santos@inmetrics.com.br>
2022-05-24 07:57:24 -0300

    Fim 3 aula - header e buscador

---
+++ src/pages/Menu/Search/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { CgSearch } from "react-icons/cg";
+import styles from "./Search.module.scss";
+
+interface SearchProps {
+    search: string;
+    setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function Search({ search, setSearch }: SearchProps) {
+    return (
+        <div className={styles.buscador}>
+            <input value={search} onChange={(event) => setSearch(event.target.value)} />
+            <CgSearch size={20} color='#4C4D5E' />
+        </div>
+    );
+}
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,12 +1,23 @@
 import styles from "./Menu.module.scss";
-import {ReactComponent as Logo} from "assets/logo.svg"
+import { ReactComponent as Logo } from "assets/logo.svg";
+import Search from "./Search";
+import { useState } from "react";
 
-export default function Menu(){
+export default function Menu() {
+    const [search, setSearch] = useState("");
+
     return (
         <main>
             <nav className={styles.menu}>
-               <Logo />
+                <Logo />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>A casa do código e da massa.</div>
+            </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__title}>Cardápio</h3>
+                <Search search={search} setSearch={setSearch} />
+            </section>
         </main>
     );
 }
